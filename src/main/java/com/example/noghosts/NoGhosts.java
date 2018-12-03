71c7b84355d44e3c395d611e997add275a1145ac
jsbarlow <joesbarlow@yahoo.com>
2018-12-02 19:34:54 -0600

    Initial Commit
    Based off of forge example

---
+++ src/main/java/com/example/noghosts/NoGhosts.java
@@ -0,0 +1,32 @@
+package com.example.noghosts;
+
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = NoGhosts.MODID, name = NoGhosts.NAME, version = NoGhosts.VERSION)
+public class NoGhosts {
+
+	public static final String MODID = "noghosts";
+    public static final String NAME = "No Ghosts";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
